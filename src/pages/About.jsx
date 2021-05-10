8b1edab2eb4788a1c7f794127d36c8ac837fd626
Moe Kassem <74279957+mkassem917@users.noreply.github.com>
2021-05-10 13:40:41 -0400

    updated the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "../assets/About.css";
 
 function About() {
-    return (
-        <div id="about">
-            <h1>Hello About Page</h1>
-        </div>
-    )
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <div id="about">
+            <h3>
+              My name is Mohammad Kassem, I'm 42 years old, married with 2 young
+              boys. I have a Bachelors degree in IT and I currently hold a
+              Senior IT Manager position with the Beef Jerky Outlet, Franchise
+              INC. Currently studying to become a full stack developer. My
+              family owns and operates a Beef Jerky Outlet Franchise in Dundee,
+              MI.
+            </h3>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default About
+export default About;
